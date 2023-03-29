54307bdda2588c42ded4649fbdf48d2eafedf42a
BrindaPuri <brindapuri@gmail.com>
2023-03-29 13:15:04 -0700

    Initial Commit

---
+++ todo_app/todo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_appApp.swift
+//  todo_app
+//
+//  Created by Brinda Puri on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct todo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
